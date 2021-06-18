e06f6ed85aaf05c978b0a2d7fc103c50d0bcb956
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-06-19 01:47:04 +0530

    Adding firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,9 @@
+
+import firebase from 'firebase'   
+import 'firebase/firestore'
+import 'firebase/auth'
+import"firebase/"
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAMaeJqpQYfUXnBTosrXI44bscjH2LYgKs",
@@ -8,3 +14,11 @@ const firebaseConfig = {
     appId: "1:623899443090:web:2a7b9ba07fbdcfda0d8b4b",
     measurementId: "G-DYPKVYNL0J"
   };
+   const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+   const db  = firebaseApp.firestore();
+   const auth = firebaseApp.auth();
+   const provider = new firebase.auth.GoogleAuthProvider();
+
+   export{ auth, provider};
+   export default db;
