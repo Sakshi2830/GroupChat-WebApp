cf415487a0bfc8e71e7f3f22ece3601b71c34ad1
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-06-20 17:49:36 +0530

    add: latest message on sidebar

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -7,6 +7,20 @@ import { Link } from 'react-router-dom';
 
 function SidebarChat({id, name, addNewChat}) {
     const [seed, setSeed] = useState("");
+    const [messages, setMessages] = useState("")
+
+    useEffect(() => {
+        if(id) {
+            db.collection("rooms")
+            .doc(id)
+            .collection("messages")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) => 
+                setMessages(snapshot.docs.map((doc) => 
+                doc.data()))
+            );
+        }
+    }, [])
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
@@ -32,13 +46,13 @@ function SidebarChat({id, name, addNewChat}) {
 
             <div className="sidebarchat__info">
                 <h2>{name}</h2>
-                <p>last message.......</p>
+                <p>{messages[0]?.message}</p>
             </div>  
         </div>
         </Link>
     ) :(
         <div className="sidebarchat" onClick={createChat} >
-            <h2>Add New Chat</h2>
+            <h3>Add New Chat</h3>
         </div>
     );
 } 
